e9650500e8417c384d241d864456cb3eb5ea3574
TakumiTosaka-cv <64624609+TakumiTosaka-cv@users.noreply.github.com>
2020-05-18 02:54:33 +0900

    Add files via upload

---
+++ builder2.js
@@ -0,0 +1,31 @@
+const columnWidth = 150
+const rowHeight = 100
+const fontSize = 56
+
+function drawTemplate(labels) {
+  const widgets = []
+  for (let rowIdx = 0; rowIdx < labels.length; rowIdx++) {
+    const rowY = rowIdx * rowHeight + (10 * rowIdx);
+    const rowLabel = labels[rowIdx];
+    if(rowLabel) {
+      widgets.push(getRowLabel(rowLabel, rowY))
+    }
+  }
+  miro.board.widgets.create(widgets)
+}
+
+function getRowLabel(text,y) {
+  return {
+    type: 'text',
+    x: -(columnWidth / 2 + fontSize),
+    y: y,
+    text: text,
+    width: columnWidth,
+    height: fontSize,
+    rotation: 0,
+    style: {
+      fontSize: fontSize,
+      textAlign: 'c'
+    }
+  }
+}
