e20d98c0f1bf5e25ed4f8a68215f5577a4cfacc6
TakumiTosaka-cv <64624609+TakumiTosaka-cv@users.noreply.github.com>
2020-05-18 14:44:23 +0900

    Update builder2.js

--- builder2.js
+++ builder2.js
@@ -1,25 +1,32 @@
 const columnWidth = 150
 const rowHeight = 100
-const fontSize = 56
+const fontSize = 36
 
 function drawTemplate(labels) {
   const widgets = []
   for (let rowIdx = 0; rowIdx < labels.length; rowIdx++) {
+    const rowY = rowIdx * rowHeight + (10 * rowIdx);
     const rowLabel = labels[rowIdx];
     if(rowLabel) {
-      widgets.push(getRowLabel(rowLabel))
+      widgets.push(getRowLabel(rowLabel, rowY))
+    }
+    
     }
   }
   miro.board.widgets.create(widgets)
 }
 
-function getRowLabel(text) {
+
+
+function getRowLabel(text, y, color) {
   return {
     type: 'text',
+    x: -(columnWidth / 2 + fontSize),
+    y: y,
     text: text,
     width: columnWidth,
     height: fontSize,
-    rotation: 0,
+    rotation: 270,
     style: {
       fontSize: fontSize,
       textAlign: 'c'
